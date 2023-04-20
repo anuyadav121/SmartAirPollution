ce450cfc6dabdb705068a66cf19f299580fdf1c2
anuyadav121 <130859191+anuyadav121@users.noreply.github.com>
2023-04-20 22:26:42 +0100

    Add files via upload

---
+++ JmdnsRegisterDiscoverUtil.java
@@ -0,0 +1,72 @@
+package util;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public class JmdnsRegisterDiscoverUtil {
+    public static Properties getProperties(String propertyName) {
+
+        Properties prop = null;
+
+        try (InputStream input = Files.newInputStream(Paths.get(propertyName))) {
+
+            prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println("alert Service properties ...");
+            System.out.println("\t service_type: " + prop.getProperty("service_type"));
+            System.out.println("\t service_name: " +prop.getProperty("service_name"));
+            System.out.println("\t service_description: " +prop.getProperty("service_description"));
+            System.out.println("\t service_port: " +prop.getProperty("service_port"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return prop;
+    }
+
+
+    public static void registerService(Properties prop) {
+
+        try {
+            // Create a JmDNS instance
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            String service_type = prop.getProperty("service_type") ;//"_http._tcp.local.";
+            String service_name = prop.getProperty("service_name")  ;// "example";
+            // int service_port = 1234;
+            int service_port = Integer.valueOf( prop.getProperty("service_port") );// #.50051;
+
+
+            String service_description_properties = prop.getProperty("service_description")  ;//"path=index.html";
+
+            // Register a service
+            ServiceInfo serviceInfo = ServiceInfo.create(service_type, service_name, service_port, service_description_properties);
+            jmdns.registerService(serviceInfo);
+
+            System.out.printf("registrering service with type %s and name %s \n", service_type, service_name);
+
+            // Wait a bit
+            Thread.sleep(1000);
+
+            // Unregister all services
+            //jmdns.unregisterAllServices();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
